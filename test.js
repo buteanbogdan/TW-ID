e12c45b520afbee980ee9a5a23681ad228e1f8db
Bogdan <b.bogdan2302@gmail.com>
2024-10-12 11:19:11 +0300

    variabile

---
+++ test.js
@@ -0,0 +1,27 @@
+function test() {
+    if (true) {
+        var var1 = 1;
+        let let1 = 2;
+        const const1 = 3;
+    }
+
+    console.log(var1);
+    console.log(let1);
+    console.log(const1);
+}
+
+// test();
+
+let let2 = 2;
+let2 = 3;
+
+// console.log(let2);
+
+const obj = {}
+
+obj.name = 'Adrian'
+
+// console.log(obj);
+
+const arr = [1,2,3,4]
+arr.push(5);
